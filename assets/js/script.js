ddd9a3b4c54c67247c399919b9b5ee3fcd3e1bc9
Sonicbasedrop <ivan.ulysses@hotmail.com>
2021-10-18 23:28:43 +0000

    add reset game variable/HTML and get cards to start game

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@
     let trimmedData = trimArray(data, level);
 
     // add two of each card to the array
-    trimmedData.forEach(function(card) {
+    trimmedData.forEach(function (card) {
       array.push(new Card(card, 1));
       array.push(new Card(card, 2));
 
@@ -88,13 +88,13 @@
   };
 
   const displayCards = cardArray => {
-    cardArray.forEach(function(card) {
+    cardArray.forEach(function (card) {
 
       // add cards to game board
       $("#game-board").append(card.html);
 
       // add click listeners
-      $(card.id).click(function() {
+      $(card.id).click(function () {
 
         // start timer on first click
         if (!gameStarted) {
@@ -109,157 +109,173 @@
       });
     });
   };
-const checkMatch = card => {
- if (!click1.name) {
-   click1 = card;
-   $(card.id).addClass("flipped");
-   return;
- 
-  } else if (!click2.name && click1.id !==card.id) {
-    click2 = card;
-    $(card.id).addClass("flipped");
+  const checkMatch = card => {
+    if (!click1.name) {
+      click1 = card;
+      $(card.id).addClass("flipped");
+      return;
 
-    // updating the moves made by player during the game
-     moves++;
-    $("#moves").text(moves);
+    } else if (!click2.name && click1.id !== card.id) {
+      click2 = card;
+      $(card.id).addClass("flipped");
 
-    checkStars();
-  } else return;
-  if (click1.name === click2.name) {
-    foundMatch();
+      // updating the moves made by player during the game
+      moves++;
+      $("#moves").text(moves);
 
-  }else {
-    hideCards();
-  }
-};
+      checkStars();
+    } else return;
+    if (click1.name === click2.name) {
+      foundMatch();
 
-const foundMatch = () => {
-  matches++;
-  if (matches === pairs) {
-    gameOver();
-  }
-  // Unbind click functions and reset click objects
-  $(click1.id).unbind("click");
-  $(click2.id).unbind("click");
+    } else {
+      hideCards();
+    }
+  };
 
-  // reset click objects
-  click1 = {};
-  click2 = {};
-};
+  const foundMatch = () => {
+    matches++;
+    if (matches === pairs) {
+      gameOver();
+    }
+    // Unbind click functions and reset click objects
+    $(click1.id).unbind("click");
+    $(click2.id).unbind("click");
 
-const hideCards = () => {
-  // hide cards
-  setTimeout(function() {
-    $(click1.id).removeClass("flipped");
-    $(click2.id).removeClass("flipped");
-    // reset click object
+    // reset click objects
     click1 = {};
     click2 = {};
-  }, 600);
-};
+  };
 
-const gameOver = () => {
-  clearInterval(timer);
+  const hideCards = () => {
+    // hide cards
+    setTimeout(function () {
+      $(click1.id).removeClass("flipped");
+      $(click2.id).removeClass("flipped");
+      // reset click object
+      click1 = {};
+      click2 = {};
+    }, 600);
+  };
 
-  //pause before showing chequred flag modal
-  setTimeout(function() {
-    $("#winModal").show();
-  }, 500);
-};
+  const gameOver = () => {
+    clearInterval(timer);
 
-const checkStars = () => {
-  let currentStars;
-  if (moves >= oneStar) {
-    currentStars = 1;
-  } else if (moves >= twoStar) {
-    currentStars = 2;
- } else currentStars = 3;
- if (numStars !== currentStars) {
-   displayStars(currentStars);
- }
-};
+    //pause before showing chequred flag modal
+    setTimeout(function () {
+      $("#winModal").show();
+    }, 500);
+  };
 
-const gameTimer = () => {
-  let startTime = new Date().getTime();
-
-  // update timer every second
-  timer = setInterval(function() {
-    var now = new Date().getTime();
-
-    // Find the time elapsed between now and start
-    var elapsed = now - startTime;
-
-    // Calculate min and sec
-    let minutes = Math.floor((elapsed % (1000 * 60 * 60)
-    ) / (1000 * 60));
-    let seconds = Math.floor((elapsed % (1000 * 60)) /
-    1000);
-
-    // add starting 0 if seconds < 10
-    if (seconds < 10) {
-      seconds = "0" + seconds;
+  const checkStars = () => {
+    let currentStars;
+    if (moves >= oneStar) {
+      currentStars = 1;
+    } else if (moves >= twoStar) {
+      currentStars = 2;
+    } else currentStars = 3;
+    if (numStars !== currentStars) {
+      displayStars(currentStars);
     }
-    let currenTime = minutes + ":" + seconds;
+  };
 
-    $(".clock").text(currentTime);
-  }, 750);
-};
+  const gameTimer = () => {
+    let startTime = new Date().getTime();
 
-   // add stars to game screen and modal
-const displayStars = num => {
-  const starImage = '<img src="assets/images/rating-star.png">';
-  $(".stars").empty();
-  for (let i = 0; i < num; i++) {
-    $(".stars").append(starImage);
-  }
-};
+    // update timer every second
+    timer = setInterval(function () {
+      var now = new Date().getTime();
 
-// open start modal on load
-$(window).on("load", function() {
-  $("#startModal").show();
-});
+      // Find the time elapsed between now and start
+      var elapsed = now - startTime;
 
-// open chequerd flag modal when game is won
-$("#openModal").click(function() {
-  $("#winModal").show();
-});
+      // Calculate min and sec
+      let minutes = Math.floor((elapsed % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((elapsed % (1000 * 60)) / 1000);
 
-// close modals when player click outside modal
-$("#winModal #close-win, #overlay").click(function() {
-  $("#winModal").hide();
-});
+      // add starting 0 if seconds < 10
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+      let currentTime = minutes + ":" + seconds;
 
-$("#startModal #close-start, #overlay").click(function() {
-  $("#startModal").hide();
-});
+      $(".clock").text(currentTime);
+    }, 750);
+  };
 
-$(".modal").click(function(event) {
-  $(".modal").hide();
-});
+  // add stars to game screen and modal
+  const displayStars = num => {
+    const starImage = '<img src="assets/images/rating-star.png">';
+    $(".stars").empty();
+    for (let i = 0; i < num; i++) {
+      $(".stars").append(starImage);
+    }
+  };
 
-$(".modal.content").click(function(event) {
-  event.stopPropagation();
-});
+  // open start modal on load
+  $(window).on("load", function () {
+    $("#startModal").show();
+  });
 
-// modal levels
-$("#easy-level").click(function() {
-  startGame(cardData, "easy");
-});
+  // open chequerd flag modal when game is won
+  $("#openModal").click(function () {
+    $("#winModal").show();
+  });
 
-$("#medium-level").click(function() {
-  startGame(cardData, "medium");
-});
+  // close modals when player click outside modal
+  $("#winModal #close-win, #overlay").click(function () {
+    $("#winModal").hide();
+  });
 
-$("#hard-level").click(function() {
-  startGame(cardData, "hard");
-});
+  $("#startModal #close-start, #overlay").click(function () {
+    $("#startModal").hide();
+  });
 
-$("#restart").click(function() {
-  $("#winModal").hide();
-  $("#startModal").show();
-});
+  $(".modal").click(function (event) {
+    $(".modal").hide();
+  });
 
+  $(".modal.content").click(function (event) {
+    event.stopPropagation();
+  });
 
+  // modal levels
+  $("#easy-level").click(function () {
+    startGame(cardData, "easy");
+  });
 
+  $("#medium-level").click(function () {
+    startGame(cardData, "medium");
+  });
 
+  $("#hard-level").click(function () {
+    startGame(cardData, "hard");
+  });
+
+  // game restart
+  $("#restart").click(function () {
+    $("#winModal").hide();
+    $("#startModal").show();
+  });
+
+  const startGame = (cards, level) => {
+    // reset game variables
+    gameStarted = false;
+    moves = 0;
+    matches = 0;
+    setLevel(level);
+
+    // reset HTML
+    $(".clock").text("0:00");
+    $("#moves").text("0");
+    $("#winModal").hide();
+
+    // get cards and start the game
+    let cardArray = makeCardArray(cardData, level);
+
+    shuffle(cardArray);
+    displayCards(cardArray);
+    displayStars(3);
+
+  };
 })();
