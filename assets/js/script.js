331e93fa1a388fedabde62baa5f76153579fd340
Sonicbasedrop <ivan.ulysses@hotmail.com>
2021-10-18 15:03:23 +0000

    add code to randomize shuffle

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,8 +66,18 @@
     });
 
     return array;
-      
-    
+  };
+
+  const shuffle = array => {
+    let currentIndex = array.length,
+    temporaryvalue,
+    randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() *
+      currentIndex);
+      currentIndex -= 1;
+    }
   }
 
 
